a58eca3604044cf7e4fe4e61c776a7de29c39b19
Nolan J Tait <nolanjtait@gmail.com>
2024-05-03 12:10:05 -0700

    Refactors copying public files in builder

--- lib/builder.rb
+++ lib/builder.rb
@@ -18,21 +18,7 @@ class Builder
     compile("index.html") { Pages::Home.new.call }
     compile("404.html") { Pages::NotFound.new.call }
     compile("500.html") { Pages::ServiceError.new.call }
-    copy("public/robots.txt", "robots.txt")
-    copy("public/sitemap.xml", "sitemap.xml")
-    copy(
-      "public/android-chrome-192x192.png",
-      "android-chrome-192x192.png"
-    )
-    copy(
-      "public/android-chrome-512x512.png",
-      "android-chrome-512x512.png"
-    )
-    copy("public/apple-touch-icon.png", "apple-touch-icon.png")
-    copy("public/favicon-16x16.png", "favicon-16x16.png")
-    copy("public/favicon-32x32.png", "favicon-32x32.png")
-    copy("public/favicon.ico", "favicon.ico")
-    copy("public/site.webmanifest", "site.webmanifest")
+    copy_public_files
   end
 
   def watch
@@ -41,6 +27,12 @@ class Builder
   end
 
   private
+
+  def copy_public_files
+    Dir["public/*"].each do |file|
+      copy(file, file.gsub("public/", ""))
+    end
+  end
 
   def listener
     call
