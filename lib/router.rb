4803fad6b6a80564c1ea29e25828f7bd2da0eba3
Nolan J Tait <nolanjtait@gmail.com>
2024-05-03 15:28:09 -0700

    Adds note about router

--- lib/router.rb
+++ lib/router.rb
@@ -3,6 +3,9 @@
 class Router
   # DOCS: Holds routes as a class instance variable. This class is expected to
   # be used as a singleton by requiring the "routes.rb" file.
+  #
+  # NOTE: Why do we need our own router? Why not just use Roda for these
+  # definitions? Roda is a routing tree and cannot be introspected easily.
 
   @routes = {}
 
