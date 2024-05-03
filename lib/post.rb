fe32d03ed86e05b39a769fd5506d17439552a21f
Nolan J Tait <nolanjtait@gmail.com>
2024-05-03 16:52:38 -0700

    Improves post with getters for frontmatter

--- lib/post.rb
+++ lib/post.rb
@@ -8,8 +8,8 @@ class Post < Protos::Markdown
         h1 { front_matter[:title] }
         span(class: "text-sm") do
           plain "Last updated: "
-          time(datetime: front_matter[:date].iso8601) do
-            front_matter[:date].strftime("%B %d, %Y")
+          time(datetime: date.iso8601) do
+            date.strftime("%B %d, %Y")
           end
         end
       end
@@ -19,10 +19,10 @@ class Post < Protos::Markdown
       end
 
       footer do
-        if front_matter[:sources] && front_matter[:sources].any?
+        if sources.any?
           h2 { "Read more" }
           ul(class: "overflow-x-auto") do
-            front_matter[:sources].each do |href|
+            sources.each do |href|
               li do
                 a(href:, class: "link") { href }
               end
@@ -31,5 +31,15 @@ class Post < Protos::Markdown
         end
       end
     end
+  end
+
+  private
+
+  def date
+    front_matter[:date] || Date.today
+  end
+
+  def sources
+    front_matter[:sources] || []
   end
 end
