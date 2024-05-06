19926fea81524614335c027ff5f677e521efdc53
Nolan J Tait <nolanjtait@gmail.com>
2024-05-06 15:55:11 -0700

    Moves vite helpers to component

--- lib/component.rb
+++ lib/component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Component < Protos::Component
+  include ViteHelpers
+
   def icon(...)
     render Icon.new(...)
   end
--- lib/layout.rb
+++ lib/layout.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Layout < Component
-  include ViteHelpers
 end
