7a085bf92320104668d5ddc1ac93ae8dc2ed360f
Nolan J Tait <nolanjtait@gmail.com>
2024-05-06 16:03:41 -0700

    Applies rubocop lint

--- app/components/navbar.rb
+++ app/components/navbar.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Components
   class Navbar < Component
     def view_template
--- app/layouts/post.rb
+++ app/layouts/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Layouts
   class Post < Layout
     def view_template(...)
