2993e5c4c0ba77e1d67d99aa8412ea461dfad31f
Nolan J Tait <nolanjtait@gmail.com>
2024-07-10 22:38:53 -0700

    Removes comment

--- gems/staticky/lib/staticky/container.rb
+++ gems/staticky/lib/staticky/container.rb
@@ -10,10 +10,6 @@ module Staticky
     setting :build_path, default: Pathname.new("build")
     setting :root_path, default: Pathname(__dir__)
 
-    # configure do |config|
-    #   config.root = Pathname(__dir__).join("..").expand_path
-    # end
-
     register :server, Staticky::Server.freeze.app
     register :router, Staticky::Router.new
     register :files, Staticky::Files.real
