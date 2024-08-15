6d92b6e694b9a63ab9b3bdcbac348dd0e85ea727
Nolan J Tait <nolanjtait@gmail.com>
2024-08-14 19:20:20 -0700

    Adds JS setup with turbo

--- frontend/entrypoints/application.js
+++ frontend/entrypoints/application.js
@@ -1,16 +1,8 @@
-// To see this message, follow the instructions for your Ruby framework.
-//
-// When using a plain API, perhaps it's better to generate an HTML entrypoint
-// and link to the scripts and stylesheets, and let Vite transform it.
-console.log('Vite ⚡️ Ruby')
-
-// Example: Import a stylesheet in <sourceCodeDir>/index.css
-// import '~/index.css'
-import '~/stylesheets/application.css'
-import "~/stylesheets/syntax.css"
 import "@fontsource-variable/inter"
 import "protos-stimulus"
 
-import { Application } from "@hotwired/stimulus"
+import "~/stylesheets/application.css"
+import "~/stylesheets/syntax.css"
 
-window.Stimulus = Application.start()
+import "~/controllers"
+import "~/turbo_transitions.js"
